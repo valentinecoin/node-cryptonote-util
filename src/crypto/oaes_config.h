6bde83334f2677bb4d16f88749f8bba0d05958d7
valentinecoin <36106470+valentinecoin@users.noreply.github.com>
2018-04-04 05:54:55 +0200

    Monero7

--- src/crypto/oaes_config.h
+++ src/crypto/oaes_config.h
@@ -39,12 +39,12 @@ extern "C" {
 //#define OAES_HAVE_ISAAC 1
 //#endif // OAES_HAVE_ISAAC
 
-#ifndef OAES_DEBUG
-#define OAES_DEBUG 0
-#endif // OAES_DEBUG
+//#ifndef OAES_DEBUG
+//#define OAES_DEBUG 0
+//#endif // OAES_DEBUG
 
 #ifdef __cplusplus 
 }
 #endif
 
-#endif // _OAES_CONFIG_H
+#endif // _OAES_CONFIG_H 
