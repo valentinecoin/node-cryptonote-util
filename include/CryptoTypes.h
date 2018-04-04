4f4b3f0c672873cfba31a99685432cb57ac59c64
valentinecoin <36106470+valentinecoin@users.noreply.github.com>
2018-04-04 06:31:12 +0200

    Monero7

---
+++ include/CryptoTypes.h
@@ -0,0 +1,42 @@
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <cstdint>
+
+namespace Crypto {
+
+struct Hash {
+  uint8_t data[32];
+};
+
+struct PublicKey {
+  uint8_t data[32];
+};
+
+struct SecretKey {
+  uint8_t data[32];
+};
+
+struct KeyDerivation {
+  uint8_t data[32];
+};
+
+struct KeyImage {
+  uint8_t data[32];
+};
+
+struct Signature {
+  uint8_t data[64];
+};
+
+}
