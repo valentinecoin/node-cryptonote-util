0f6e7ee546f0236a3ee05b337576cc8abe596377
valentinecoin <36106470+valentinecoin@users.noreply.github.com>
2018-04-04 09:32:22 +0200

    Monero7

--- src/cryptonote_core/cryptonote_format_utils.cpp
+++ src/cryptonote_core/cryptonote_format_utils.cpp
@@ -15,6 +15,12 @@ using namespace epee;
 
 namespace cryptonote
 {
+	std::stringstream& operator<<(std::stringstream& out, const std::wstring& ws)
+{
+	std::string as = string_encoding::convert_to_ansii(ws);
+	out << as;
+	return out;
+}
 	//---------------------------------------------------------------
 	void get_transaction_prefix_hash(const transaction_prefix& tx, Crypto::Hash& h)
 	{
