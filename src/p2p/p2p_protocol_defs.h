446b7297d3b01d38e5cea1d54df28ef210741c49
valentinecoin <36106470+valentinecoin@users.noreply.github.com>
2018-04-04 06:22:31 +0200

    Monero7

--- src/p2p/p2p_protocol_defs.h
+++ src/p2p/p2p_protocol_defs.h
@@ -204,7 +204,7 @@ namespace nodetool
   {
     peerid_type peer_id;
     uint64_t    time;
-    crypto::signature sign;
+    Crypto::signature sign;
 
     BEGIN_KV_SERIALIZE_MAP()
       KV_SERIALIZE(peer_id)
@@ -312,4 +312,4 @@ namespace nodetool
 }
 
 
-
+ 
